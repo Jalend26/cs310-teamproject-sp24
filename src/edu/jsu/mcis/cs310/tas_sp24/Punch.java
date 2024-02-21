cf296895809b4be3f0dde093a6d3ed73496ac4a3
Jalend26 <157413998+Jalend26@users.noreply.github.com>
2024-02-21 13:27:42 -0600

    Update Punch.java

--- src/edu/jsu/mcis/cs310/tas_sp24/Punch.java
+++ src/edu/jsu/mcis/cs310/tas_sp24/Punch.java
@@ -2,4 +2,23 @@ package edu.jsu.mcis.cs310.tas_sp24;
 
 public class Punch {
     
+    private final String termid, badge, punchType;
+    
+    public Punch(String termid, String badge, String punchType) {
+        this.termid = termid;
+        this.badge = badge;
+        this.punchType = punchType;
+    }
+    
+    public String gettermid() {
+        return termid;
+    }
+    
+    public String getbadge() {
+        return badge;
+    }
+    
+    public String getpunchType() {
+        return punchType;
+    }
 }
