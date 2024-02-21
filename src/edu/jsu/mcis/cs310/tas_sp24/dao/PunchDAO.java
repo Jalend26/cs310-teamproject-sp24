882910cddc362cacb4c61bcfa1c1587a32565ee1
Jalend26 <157413998+Jalend26@users.noreply.github.com>
2024-02-21 12:58:23 -0600

    Update PunchDAO.java

--- src/edu/jsu/mcis/cs310/tas_sp24/dao/PunchDAO.java
+++ src/edu/jsu/mcis/cs310/tas_sp24/dao/PunchDAO.java
@@ -3,4 +3,10 @@ package edu.jsu.mcis.cs310.tas_sp24.dao;
 
 public class PunchDAO {
     
+    private final DAOFactory daoFactory;
+    
+    PunchDAO(DAOFactory daoFactory) {
+        this.daoFactory = daoFactory;
+    }
+    
 }
