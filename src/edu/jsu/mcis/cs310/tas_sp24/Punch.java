8518b8fa01b18d8d507caaba3ec82ef00f8ecef9
Jalend26 <157413998+Jalend26@users.noreply.github.com>
2024-02-23 12:48:54 -0600

    Update Punch Model Class

--- src/edu/jsu/mcis/cs310/tas_sp24/Punch.java
+++ src/edu/jsu/mcis/cs310/tas_sp24/Punch.java
@@ -2,7 +2,7 @@ package edu.jsu.mcis.cs310.tas_sp24;
 
 public class Punch {
     
-    private final String termid, badge, punchType;
+    private final String termid, badge, punchType, id, localDateTime;
     
     public Punch(String termid, String badge, String punchType) {
         this.termid = termid;
@@ -22,10 +22,32 @@ public class Punch {
         return punchType;
     }
     
+    public Punch(String id, String termid, String badge, String localDateTime, String punchType) {
+        this.id = id;
+        this.localDateTime = localDateTime;
+        this.termid = termid;
+        this.badge = badge;
+        this.punchType = punchType;
+        
+    }
+    
+    public String getid(){
+        return id;
+    }
+    public String getlocalDateTime() {
+        return localDateTime;
+    }
+    
     @Override
     public String toString() {
         
         StringBuilder p = new StringBuilder();
+        
+        p.append('#').append(termid).append(' ');
+        p.append('(').append(badge).append(')');
+        p.append('(').append(punchType).append(')');
+        
+        return p.toString();
         
     }
 }
--- src/edu/jsu/mcis/cs310/tas_sp24/dao/PunchDAO.java
+++ src/edu/jsu/mcis/cs310/tas_sp24/dao/PunchDAO.java
@@ -1,5 +1,8 @@
 package edu.jsu.mcis.cs310.tas_sp24.dao;
 
+import edu.jsu.mcis.cs310.tas_sp24.Badge;
+import edu.jsu.mcis.cs310.tas_sp24.EventType;
+
 
 public class PunchDAO {
     
@@ -7,6 +10,16 @@ public class PunchDAO {
     
     PunchDAO(DAOFactory daoFactory) {
         this.daoFactory = daoFactory;
+    }
+    
+    public String find(int termid, Badge badge, EventType punchType) {
+        
+        String result = "[]";
+        
+        
+        return null;
+        
+        
     }
     
 }
