69ba27c4fb8b250f5b89be12dce3a768fb2f9e3b
Daniel <dholcombe1@stu.jsu.edu>
2024-02-26 12:45:33 -0600

    PunchDAO and DAOException edited

--- src/edu/jsu/mcis/cs310/tas_sp24/dao/DAOException.java
+++ src/edu/jsu/mcis/cs310/tas_sp24/dao/DAOException.java
@@ -4,6 +4,11 @@ public class DAOException extends RuntimeException {
 
     public DAOException(String message) {
         super(message);
+
+    }
+
+    public DAOException(String message, Throwable cause) {
+        super(message, cause);
     }
 
 }
