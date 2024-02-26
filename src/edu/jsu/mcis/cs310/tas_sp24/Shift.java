0b4d123ea23c2a8620a6807738cf453755636375
unknown <wahickssware84@gmail.com>
2024-02-26 11:58:09 -0600

    Initial Commit; added skeleton for Shift and ShiftDAO classes (more to come later this week).

--- src/edu/jsu/mcis/cs310/tas_sp24/Shift.java
+++ src/edu/jsu/mcis/cs310/tas_sp24/Shift.java
@@ -1,5 +1,13 @@
 package edu.jsu.mcis.cs310.tas_sp24;
 
 public class Shift {
-    
+
+    // in development - William
+
+    public String toString() {
+        StringBuilder s = new StringBuilder();
+
+        // in development - William
+
+        return s.toString();
 }
--- src/edu/jsu/mcis/cs310/tas_sp24/dao/ShiftDAO.java
+++ src/edu/jsu/mcis/cs310/tas_sp24/dao/ShiftDAO.java
@@ -1,6 +1,11 @@
 package edu.jsu.mcis.cs310.tas_sp24.dao;
 
-
 public class ShiftDAO {
-    
+
+    // in development - William
+
+    private final DAOFactory daoFactory;
+    PunchDAO(DAOFactory daoFactory) {
+            this.daoFactory = daoFactory;
+        }
 }
