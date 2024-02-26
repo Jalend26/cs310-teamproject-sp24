922b8f789fa2211084f3395e3497313137d08c2f
Jalend26 <157413998+Jalend26@users.noreply.github.com>
2024-02-26 13:22:47 -0600

    Punch Model Class Complete

--- src/edu/jsu/mcis/cs310/tas_sp24/Punch.java
+++ src/edu/jsu/mcis/cs310/tas_sp24/Punch.java
@@ -4,7 +4,7 @@ package edu.jsu.mcis.cs310.tas_sp24;
 
 public class Punch {
     
-    private final String termid, badge, punchType;
+    private final String termid, badge, punchType, id, localDateTime;
     
     public Punch(String termid, String badge, String punchType) {
         this.termid = termid;
@@ -26,10 +26,31 @@ public class Punch {
         return punchType;
     }
     
+     public Punch(String id, String termid, String badge, String localDateTime, String punchType) {
+        this.id = id;
+        this.localDateTime = localDateTime;
+        this.termid = termid;
+        this.badge = badge;
+        this.punchType = punchType;
+        
+    }
+    
+    public String getid(){
+        return id;
+    }
+    public String getlocalDateTime() {
+        return localDateTime;
+    }
+    
+    public String printOriginal() {
+        
+        return "ID: " + id + "/nTermID: " + termid + "/nBadge: " + badge + "/nLocalDateTime: " + localDateTime + "/nPunchType: " + punchType; 
+    }
+    
     @Override
     public String toString() {
         
-        StringBuilder p = new StringBuilder();
+        return printOriginal();
         
     }
 }
