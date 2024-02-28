2a05ef315956635255160bc4e8e5bfe277f5cabb
unknown <wahickssware84@gmail.com>
2024-02-28 11:56:57 -0600

    Finished toString() and getter methods for Shift class.

--- src/edu/jsu/mcis/cs310/tas_sp24/Shift.java
+++ src/edu/jsu/mcis/cs310/tas_sp24/Shift.java
@@ -1,5 +1,71 @@
 package edu.jsu.mcis.cs310.tas_sp24;
 
+import java.util.*;
+import java.time.*;
+
 public class Shift {
-    
+    private int id;
+    private String desc;
+    private DailySchedule defaultschedule;
+
+    public int getID(){
+            return id;
+        }
+
+    public String getDescription(){
+            return desc;
+        }
+
+    public LocalTime getStartTime() {
+            return defaultschedule.getStartTime();
+        }
+
+    public LocalTime getStopTime() {
+            return defaultschedule.getStopTime();
+        }
+
+    public int getGracePeriod(){
+            return defaultschedule.getGracePeriod();
+        }
+
+    public int getDockPenalty(){
+            return defaultschedule.getDockPenalty();
+        }
+
+    public int getRoundInterval(){
+            return defaultschedule.getRoundInterval();
+        }
+
+    public LocalTime getLunchStart() {
+            return defaultschedule.getLunchStart();
+        }
+
+    public LocalTime getLunchStop() {
+            return defaultschedule.getLunchStop();
+        }
+
+    public int getLunchThreshold(){
+            return defaultschedule.getLunchThreshold();
+        }
+
+    public Duration getLunchDuration() {
+            return defaultschedule.getLunchDuration();
+        }
+
+    public Duration getShiftDuration() {
+            return defaultschedule.getShiftDuration();
+        }
+
+    @Override
+        public String toString() {
+            StringBuilder s = new StringBuilder();
+            s.append(desc).append(": ");
+            s.append(getStartTime()).append(" - ");
+            s.append(getStopTime()).append(" (");
+            s.append(getShiftDuration().toMinutes()).append(" minutes); Lunch: ");
+            s.append(getLunchStart()).append(" - ");
+            s.append(getLunchStop()).append(" (");
+            s.append(getLunchDuration().toMinutes()).append(" minutes)");
+            return s.toString();
+        }
 }
