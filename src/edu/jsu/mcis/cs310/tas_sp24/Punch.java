1e4fa9ea38bbb88e32916c46a6a466a7f20814f3
Jalend26 <157413998+Jalend26@users.noreply.github.com>
2024-03-01 08:26:00 -0600

    Update Punch.java
    redid the private calls and added the adjustedTime and the punchAdjustmentType

--- src/edu/jsu/mcis/cs310/tas_sp24/Punch.java
+++ src/edu/jsu/mcis/cs310/tas_sp24/Punch.java
@@ -2,49 +2,57 @@ package edu.jsu.mcis.cs310.tas_sp24;
 
 // Punch Model Class Complete - Jalen
 
+import java.time.LocalDateTime;
+
+
 public class Punch {
     
-    private final String termid, badge, punchType, id, localDateTime;
+    private LocalDateTime originalTime, adjustedTime;
+    private final EventType punchType;
+    private final int termid;
+    private int id;
+    private final Badge badge;
+    private PunchAdjustmentType adjustmentType;
     
-    public Punch(String termid, String badge, String punchType) {
-        this.termid = termid;
-        this.badge = badge;
-        this.punchType = punchType;
-        this.id = null;
-        this.localDateTime = null;
-    }
     
-    public String gettermid() {
-        return termid;
-    }
-    
-    public String getbadge() {
-        return badge;
-    }
-    
-    public String getpunchType() {
-        return punchType;
-    }
-    
-     public Punch(String id, String termid, String badge, String localDateTime, String punchType) {
-        this.id = id;
-        this.localDateTime = localDateTime;
+    public Punch(int termid, Badge badge, EventType punchType) {
         this.termid = termid;
         this.badge = badge;
         this.punchType = punchType;
         
     }
     
-    public String getid(){
+    public int gettermid() {
+        return termid;
+    }
+    
+    public Badge getbadge() {
+        return badge;
+    }
+    
+    public EventType getpunchType() {
+        return punchType;
+    }
+    
+    public Punch(int id, int termid, Badge badge, LocalDateTime originalTime, EventType punchType) {
+        this.id = id;
+        this.originalTime = originalTime;
+        this.termid = termid;
+        this.badge = badge;
+        this.punchType = punchType;
+        
+    }
+    
+    public int getid(){
         return id;
     }
-    public String getlocalDateTime() {
-        return localDateTime;
+    public LocalDateTime getoriginalTime() {
+        return originalTime;
     }
     
     public String printOriginal() {
         
-        return "ID: " + id + "/nTermID: " + termid + "/nBadge: " + badge + "/nLocalDateTime: " + localDateTime + "/nPunchType: " + punchType; 
+        return "ID: " + id + "/nTermID: " + termid + "/nBadge: " + badge + "/nLocalDateTime: " + originalTime + "/nPunchType: " + punchType; 
     }
     
     @Override
