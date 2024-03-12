1e7a60f9c5a02d6aaa6cfd7fac69324cd334c5d9
unknown <wahickssware84@gmail.com>
2024-03-12 18:22:33 -0500

    Formatted & applied preliminary changes to EmployeeDAO.java and DepartmentDAO.java. Next commit will most likely be within 2-4 hours.

--- src/edu/jsu/mcis/cs310/tas_sp24/dao/DepartmentDAO.java
+++ src/edu/jsu/mcis/cs310/tas_sp24/dao/DepartmentDAO.java
@@ -1,9 +1,11 @@
 package edu.jsu.mcis.cs310.tas_sp24.dao;
 
 public class DepartmentDAO {
-    private final DAOFactory daofactory;
-    
-    public DepartmentDAO(DAOFactory daofactory) {
-        this.daofactory = daofactory;
+
+    private static final String QUERY_FIND = "SELECT * FROM department WHERE id = ?";
+    private final DAOFactory daoFactory;
+
+    public DepartmentDAO(DAOFactory daoFactory) {
+        this.daoFactory = daoFactory;
     }
 }
--- src/edu/jsu/mcis/cs310/tas_sp24/dao/EmployeeDAO.java
+++ src/edu/jsu/mcis/cs310/tas_sp24/dao/EmployeeDAO.java
@@ -1,9 +1,11 @@
 package edu.jsu.mcis.cs310.tas_sp24.dao;
 
-/**
- *
- * @author jalen
- */
 public class EmployeeDAO {
-    
+
+    private static final String QUERY_FIND = "SELECT * FROM employee WHERE id = ?";
+    private final DAOFactory daoFactory;
+
+    public EmployeeDAO(DAOFactory daoFactory) {
+        this.daoFactory = daoFactory;
+    }
 }
