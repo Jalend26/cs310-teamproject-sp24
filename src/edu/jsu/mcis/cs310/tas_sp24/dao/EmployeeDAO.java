37cde9d813e11173b0e4e74460015848af4977c1
unknown <wahickssware84@gmail.com>
2024-03-12 20:41:44 -0500

    Finished DepartmentDAO.java & EmployeeDAO.java. The inactive field is to be ignored for now (as per the instructions).

--- src/edu/jsu/mcis/cs310/tas_sp24/dao/EmployeeDAO.java
+++ src/edu/jsu/mcis/cs310/tas_sp24/dao/EmployeeDAO.java
@@ -1,11 +1,97 @@
 package edu.jsu.mcis.cs310.tas_sp24.dao;
 
+import edu.jsu.mcis.cs310.tas_sp24.Badge;
+import edu.jsu.mcis.cs310.tas_sp24.Department;
+import edu.jsu.mcis.cs310.tas_sp24.EmployeeType;
+import edu.jsu.mcis.cs310.tas_sp24.Shift;
+import edu.jsu.mcis.cs310.tas_sp24.Employee;
+import java.time.LocalDateTime;
+import java.sql.*;
+
 public class EmployeeDAO {
 
-    private static final String QUERY_FIND = "SELECT * FROM employee WHERE id = ?";
+    private static final String QUERY_FIND = "SELECT * FROM employee where id = ?";
+    private static final String QUERY_FIND_1 = "SELECT * FROM employee where badgeid = ?";
+
     private final DAOFactory daoFactory;
 
-    public EmployeeDAO(DAOFactory daoFactory) {
+    EmployeeDAO(DAOFactory daoFactory) {
         this.daoFactory = daoFactory;
+    }
+
+    public Employee find(int id) {
+
+        Employee employee = null;
+
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+
+        try {
+
+            Connection conn = daoFactory.getConnection();
+
+            if (conn.isValid(0)) {
+
+                ps = conn.prepareStatement(QUERY_FIND);
+                ps.setInt(1, id);
+
+                boolean hasresults = ps.execute();
+
+                if (hasresults) {
+
+                    rs = ps.getResultSet();
+
+                    while (rs.next()) {
+
+                        id = rs.getInt("id");
+                        int employeetypeid = rs.getInt("employeetypeid");
+                        String firstName = rs.getString("firstname");
+                        String middleName = rs.getString("middlename");
+                        String lastName = rs.getString("lastname");
+
+                        BadgeDAO badgeDAO = daoFactory.getBadgeDAO();
+                        String badgeid = rs.getString("badgeid");
+                        Badge badge = badgeDAO.find(badgeid);
+
+                        DepartmentDAO departmentDAO = daoFactory.getDepartmentDAO();
+                        int departmentid = rs.getInt("departmentid");
+                        Department department = departmentDAO.find(departmentid);
+
+                        ShiftDAO shiftDAO = daoFactory.getShiftDAO();
+                        int shiftid = rs.getInt("shiftid");
+                        Shift shift = shiftDAO.find(shiftid);
+
+                        EmployeeType employeeType = EmployeeType.values()[employeetypeid];
+
+                        employee = new Employee(id, firstName, middleName, lastName, inactive, badge, department, shift, employeeType);
+
+                    }
+                }
+            }
+
+        } catch (SQLException e) {
+
+            throw new DAOException(e.getMessage());
+
+        } finally {
+
+            if (rs != null) {
+                try {
+                    rs.close();
+                } catch (SQLException e) {
+                    throw new DAOException(e.getMessage());
+                }
+            }
+            if (ps != null) {
+                try {
+                    ps.close();
+                } catch (SQLException e) {
+                    throw new DAOException(e.getMessage());
+                }
+            }
+
+        }
+
+        return employee;
     }
 }
