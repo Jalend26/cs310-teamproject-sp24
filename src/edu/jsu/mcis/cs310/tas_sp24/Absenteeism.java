ead05a88f4a23061f05469f097c628d33330db8b
Jalend26 <157413998+Jalend26@users.noreply.github.com>
2024-04-08 13:01:31 -0500

    AbSeNtEeIsM MoDeL ClAsS

--- src/edu/jsu/mcis/cs310/tas_sp24/Absenteeism.java
+++ src/edu/jsu/mcis/cs310/tas_sp24/Absenteeism.java
@@ -1,12 +1,55 @@
 package edu.jsu.mcis.cs310.tas_sp24;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
 /**
  *
  * @author jalen
  */
 public class Absenteeism {
     
-    private final String id;
+    private final Employee employeeid;
     private final LocalDate payperiod;
+    private final BigDecimal percentage;
+    
+    public Absenteeism(Employee employeeid, LocalDate payperiod, BigDecimal percentage) {
+        
+        this.employeeid = employeeid;
+        this.payperiod = payperiod;
+        this.percentage = percentage; 
+        
+    }
+    
+    public Employee getEmployeeid() {
+    
+        return employeeid;
+        
+    }
+    
+    public LocalDate getPayperiod() {
+        
+        return payperiod;
+        
+    }
+    
+    public BigDecimal getPercentage() {
+        
+        return percentage; 
+        
+    }
+    
+    @Override
+    public String toString() {
+        
+        StringBuilder s = new StringBuilder();
+        
+        s.append("# ").append(employeeid).append(" ");
+        s.append("(Pay Period Starting ").append(payperiod).append(")");
+        s.append(": ").append(percentage);
+        
+        return s.toString();
+        
+    }
     
 }
--- src/edu/jsu/mcis/cs310/tas_sp24/dao/DAOUtility.java
+++ src/edu/jsu/mcis/cs310/tas_sp24/dao/DAOUtility.java
@@ -5,6 +5,9 @@ import java.util.*;
 import java.time.temporal.ChronoUnit;
 import java.time.format.DateTimeFormatter;
 import com.github.cliftonlabs.json_simple.*;
+import edu.jsu.mcis.cs310.tas_sp24.Punch;
+import edu.jsu.mcis.cs310.tas_sp24.Shift;
+import java.util.ArrayList;
 
 /**
  * 
@@ -13,6 +16,30 @@ import com.github.cliftonlabs.json_simple.*;
  * individual static methods.
  * 
  */
-public final class DAOUtility {
+public class DAOUtility {
 
+    public static int calculateTotalMinutes(ArrayList<Punch> dailypunchlist, Shift shift) {
+        int totalMinutes = 0;
+        boolean lunchDeducted = false;
+
+        for (int i = 0; i < dailypunchlist.size() - 1; i++) {
+            Punch inPunch = dailypunchlist.get(i);
+            Punch outPunch = dailypunchlist.get(i + 1);
+
+            // Check if the pair is a valid clock in and clock out punch
+            if (inPunch.isClockIn() && outPunch.isClockOut()) {
+                int minutes = (int) ((outPunch.getPunchTime().getTime() - inPunch.getPunchTime().getTime()) / (60 * 1000));
+                totalMinutes += minutes;
+                i++; // Skip the next punch as it has already been processed
+            }
+        }
+
+        // Deduct lunch break if applicable
+        if (totalMinutes > shift.getLunchThreshold() && !lunchDeducted) {
+            totalMinutes -= shift.getLunchBreakDuration();
+            lunchDeducted = true;
+        }
+
+        return totalMinutes;
+    }
 }
